57e9ef6f74de331f986366ffbc5ba772bd6f5c10
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-07 12:02:18 +0530

    initial commit first problem on sum.

---
+++ practice01.py
@@ -0,0 +1,10 @@
+#Solving the problem.
+
+# 01. Write a program to obtain three numbers and print their sum.
+
+number1= int(input('Enter number1:'))
+number2= int(input('Enter number2:'))
+number3= int(input('Enter number3:'))
+sum= number1+number2+number3
+print("Three numbers are",'number1','number2','number3')
+print("Sum of three number is", sum)
