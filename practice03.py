6a140797e475b223e7f50729aca4dbbdc09ded6a
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-07 12:17:54 +0530

    To find the cube.

---
+++ practice03.py
@@ -0,0 +1,6 @@
+#03. Write a program to input a number and print its cube.
+
+num = float(input('Enter a number:'))
+cube = num*num*num
+print('Entered Number is: ', num)
+print("It's cube is:", cube)
