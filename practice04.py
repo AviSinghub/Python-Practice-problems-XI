ed99c1fdf4390b915f480d4a1aa166ece0165eaa
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-07 12:37:34 +0530

    Converting km into mIles.

---
+++ practice04.py
@@ -0,0 +1,12 @@
+# 04. Write a program to input a value in kilometers and convert it into miles(1 km = 0.62137)
+
+distance = int(input("Enter distance in kilometers:"))
+miles = distance*0.62137
+print("Distance in kilometers:", distance)
+print("Miles:", miles)
+
+
+distance = float(input("Enter distance in kilometers:"))
+miles = distance*0.62137
+print("Distance in kilometers:", distance)
+print("Miles:", miles)
