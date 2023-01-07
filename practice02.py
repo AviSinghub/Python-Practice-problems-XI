173a66522bebc5955bf81c5105876098e7b48cca
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-07 12:11:19 +0530

    calculate area of rectangle.

---
+++ practice02.py
@@ -0,0 +1,10 @@
+# Solve the problem.
+#02. Write a program to obtain length and breadth of a rectangle and calculate its area.
+
+length= float(input('Enter the length of the rectangle:'))
+breadth= float(input('Enter the breadth of the rectangle:'))
+area=length*breadth
+print("Rectangele dimensions")
+print('Length=', length, end=' ' )
+print('Breadth=', breadth )
+print('Area', area,) 
