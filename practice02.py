c794f6aaca6f74873eded4e2d368c5669aedfcd1
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-08 14:14:37 +0530

    subscripting of power value commented.

--- practice02.py
+++ practice02.py
@@ -1,5 +1,6 @@
 # Solve the problem.
 #02. Write a program to obtain length and breadth of a rectangle and calculate its area.
+#  (use Alt+0178 for power subscript)
 
 length= float(input('Enter the length of the rectangle:'))
 breadth= float(input('Enter the breadth of the rectangle:'))
