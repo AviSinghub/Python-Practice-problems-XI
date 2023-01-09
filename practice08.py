339d12429fd072dc18ff3bdd1e6c07608207da0e
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-09 13:26:48 +0530

    Average of five numbers.

---
+++ practice08.py
@@ -0,0 +1,10 @@
+# 08. Write a program to find average of five numbers.
+
+a = int(input("Eneter  number1:"))
+b = int(input("Eneter  number2:"))
+c = int(input("Eneter  number3:"))
+d = int(input("Eneter  number4:"))
+e = int(input("Eneter  number5:"))
+print(" Five numbers are:", a,b,c,d,e)
+avg = (a+b+c+d+e)/5
+print("Average of given numbers:", avg)
