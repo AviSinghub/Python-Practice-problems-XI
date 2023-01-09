d863d21a7c37359f876e339caabc93bbf78c7731
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-09 12:37:09 +0530

    New program on volume of a cylinder.

---
+++ practice07.py
@@ -0,0 +1,9 @@
+# 07. Write a program to find the volume of a cylinder.
+
+radius= float(input("Enter the radius:"))
+height = float(input("Enter the height:"))
+volume= 3.14*radius*radius*height
+print("Dimensions of a cylnder")
+print("Radius is:",radius,'m', end =' ' )
+print("Height is:",height, 'm')
+print("Volume of cylinder:",volume,"m³" )
