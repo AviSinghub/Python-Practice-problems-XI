b690d59e231affcc9c1e1b10fba97513c4de4b16
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-13 12:06:19 +0530

    Question on sum, with new line using'\n' .

---
+++ practice09.py
@@ -0,0 +1,15 @@
+# solving the sum of numbers using '\n' 
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+num3 = int(input("Enter the third number:"))
+num4 = int(input("Enter the fourth number:"))
+num5 = int(input("Enter the fifth number:"))
+sum = num1+num2+num3+num4+num5
+print(" You have entered the following numbers")
+print("first number:",num1, '\n' )
+print("second number:",num2, '\n' )
+print("third number:",num3, '\n' )
+print("fourth number:",num4,'\n' )
+print("fifth number:",num5, '\n' )
+print("Sum of five number is:",sum)
