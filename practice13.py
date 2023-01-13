290363f59f9fd90827cb044d2d4b821dc6f97a67
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-13 12:25:30 +0530

    Checking code with end argument.

---
+++ practice13.py
@@ -0,0 +1,5 @@
+# 13. consider the program.
+
+name = input("What is your name?")
+print('Hi',name,',', end=' ')      # end='' argument should be placed to print the next line of text.
+print("How are you doing?")
